c16f2ff6775f064cb4d4fd90676f9df2b0a513e9
TheoHappy <38621790+TheoHappy@users.noreply.github.com>
2019-12-15 23:29:31 +0300

    finish task 14.22

--- src/MorseCode.java
+++ src/MorseCode.java
@@ -2,33 +2,69 @@ import java.util.Scanner;
 
 public class MorseCode {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter sentence and press enter ");
-        String sentence = input.nextLine();
-        String[] tokens = sentence.split(" ");
+        Scanner inputDigit = new Scanner(System.in);
+        char[] englishDictionary = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+                'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
+        };
 
-        for (int i = 0; i < tokens.length; i++) {
-            for (int j = 0; j < tokens[i].length(); j++){
-                System.out.println(tokens[i].charAt(j));
-            }
-        }
-    }
-    public static String convertToMorseCode(char c){
-        String result = null;
-        switch (c){
-            case 'a':
-                result = ".-";
-                break;
-            case  'b':
-                result = "-...";
-                break;
-            case 'c':
-                result = "-.-.";
-                break;
-            case '':
-                result = "";
-                break;
+        String[] morseDictionary = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".---.", "--.-", ".-.",
+                "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..", ".----",
+                "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----.",
+                "-----"};
 
+        System.out.println();
+        System.out.println("This is an English to Morse Code Translator and Vice Versa.");
+        System.out.println("Please Enter 1 for English to Morse Code\n " +
+                            "         or 2 for Morse Code to English");
+
+        int choice = inputDigit.nextInt();
+        switch (choice) {
+            case 1:
+                System.out.println("Please enter what you would like translate into Morse Code.");
+                String sentence = input.nextLine();
+                sentence = sentence.toLowerCase();
+                String[] word = sentence.split(" ");
+
+                String englishToMorse = "";
+                for (int i = 0; i < word.length; i++) {
+                    for (int j = 0; j < word[i].length(); j++) {
+                        for (int k = 0; k < englishDictionary.length; k++) {
+                            if (englishDictionary[k] == word[i].charAt(j)) {
+                                englishToMorse += morseDictionary[k] + " ";
+                            }
+                        }
+                    }
+                    englishToMorse += "  ";
+                }
+                System.out.println(englishToMorse);
+                break;
+            case 2:
+                System.out.println("Please enter what you would like translate into English.");
+                String morseInput = input.nextLine();
+                String[] token3Blanks = morseInput.split("   ");
+
+                String morseToEnglish = "";
+                String[] token1Blank = null;
+                for (int i = 0; i < token3Blanks.length; i++) {
+                    token1Blank = token3Blanks[i].split(" ");
+                    for (int j = 0; j < token1Blank.length; j++) {
+                        for (int k = 0; k < morseDictionary.length; k++) {
+                            if (morseDictionary[k].equals(token1Blank[j])) {
+                                morseToEnglish = morseToEnglish + englishDictionary[k];
+                            }
+                        }
+                    }
+                    morseToEnglish += " ";
+                }
+                System.out.println(morseToEnglish);
+                break;
+            default:
+                System.err.println("Invalid Option Entered");
+                break;
         }
     }
 }
